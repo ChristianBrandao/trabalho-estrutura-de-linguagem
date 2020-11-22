16e97df22dff10cb31d5ee1d6ade55bdb0d29e47
Christian Xavier Brandão <christiancrmj16@gmail.com>
2020-11-21 22:36:12 -0200

    versão com complexidade 02

---
+++ trabalho01/complexidade02/bin/complexidade02.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'dart:async';
+
+Future main() async{ // Future declarado para informar uma função que retorna um valor futuro
+  String URL = 'https://jsonplaceholder.typicode.com/posts'; //Obter dados da web
+  Dio dio = new Dio(); //função DIO faz a requisição http
+  print("Inico programa");
+  print("Programa em execuçao 1");
+  await dio.get(URL).then((Response){ //programa espera a conclusão da tarefa em segundo plano
+  print(Response.data);
+ 
+  });
+  print("Programa em execuçao 2");
+  print("Fim programa"); 
+  await Future.delayed(Duration(seconds:1));//programa espera a conclusão da tarefa em segundo plano em deley de 1 seg
+}
